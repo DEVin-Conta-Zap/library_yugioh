305975cbd5691273f1ae90fbf3ab6f4d75cd761d
douglas-cavalcante <henriquedouglas2803@gmail.com>
2022-02-23 20:09:13 -0300

    adiciona api intersectionObserver

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -20,28 +20,50 @@ interface IResponseGetCards extends ICard {
 const Cards = () => {
 
   const params = useParams();
+  const [page, setPage] = useState<number>(0)
 
   const [cards, setCards] = useState<Array<ICard>>([]);
 
   useEffect(() => {
-    api.get<IResponseGetCards[]>(`cards?race=${params.type}`)
+    api.get<IResponseGetCards[]>(
+      `cards?race=${params.type}&_limit=1&_page=${page}`
+    )
       .then((response) => {
-        setCards(response.data)
+        setCards([...cards, ...response.data])
       })
       .catch((error) => console.log(error))
       .finally(() => console.log('A chamada terminou'))
-  }, [params.type]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.type, page]);
+
+  useEffect(() => {
+
+    const intersectionObserver = new IntersectionObserver(entries => {
+      if (entries.some(entry => entry.isIntersecting)) {
+        console.log(entries)
+        setPage((currentValue) => currentValue + 1);
+      }
+    })
+
+    //@ts-ignore
+    intersectionObserver.observe(document.querySelector('#sentinela'));
+    return () => intersectionObserver.disconnect();
+  }, []);
 
   return (
-    <div className='container container-cards'>
-      {
-        cards.map((card) =>
-          <Card
-            key={card.id}
-            data={card}
-          />)
-      }
-    </div>
+    <>
+      <div className='container container-cards'>
+        {
+          cards.map((card) =>
+            <Card
+              key={card.id}
+              data={card}
+            />)
+        }
+
+      </div>
+      <p style={{ background: 'red', flex: 1 }} id="sentinela">123</p>
+    </>
   )
 }
 
