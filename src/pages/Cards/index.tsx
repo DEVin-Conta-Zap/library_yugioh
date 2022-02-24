e775a63a1b09e30464bb877dff5152b9653076cb
douglas-cavalcante <henriquedouglas2803@gmail.com>
2022-02-24 19:26:51 -0300

    melhora sentinela

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Card from '../../components/Card';
 import api from '../../services/axios';
@@ -20,13 +20,25 @@ interface IResponseGetCards extends ICard {
 const Cards = () => {
 
   const params = useParams();
-  const [page, setPage] = useState<number>(0)
+  const [page, setPage] = useState<number>(1)
 
   const [cards, setCards] = useState<Array<ICard>>([]);
 
+  const [lastElement, setLastElement] = useState(null);
+
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const first = entries[0];
+        if (first.isIntersecting) {
+          setPage((currentPage) => currentPage + 1);
+        }
+      })
+  );
+
   useEffect(() => {
     api.get<IResponseGetCards[]>(
-      `cards?race=${params.type}&_limit=20&_page=${page}`
+      `cards?race=${params.type}&_limit=10&_page=${page}`
     )
       .then((response) => {
         setCards([...cards, ...response.data])
@@ -34,36 +46,46 @@ const Cards = () => {
       .catch((error) => console.log(error))
       .finally(() => console.log('A chamada terminou'))
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [params.type, page]);
+  }, [page]);
 
   useEffect(() => {
+    const currentElement = lastElement;
+    const currentObserver = observer.current;
 
-    const intersectionObserver = new IntersectionObserver(entries => {
-      if (entries.some(entry => entry.isIntersecting)) {
-        console.log(entries)
-        setPage((currentValue) => currentValue + 1);
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
       }
-    })
-
-    //@ts-ignore
-    intersectionObserver.observe(document.querySelector('#sentinela'));
-    return () => intersectionObserver.disconnect();
-  }, []);
+    };
+  }, [lastElement]);
 
   return (
     <>
       <div className='container container-cards'>
         {
-          cards.map((card) =>
-            <Card
+          cards.map((card, index) => {
+            if (cards.length - 1 === index) {
+              return (
+                //@ts-ignore
+                <div ref={setLastElement} key={card.id} >
+                  <Card
+                    data={card}
+                  />
+                </div>
+              )
+            }
+
+            return (<Card
               key={card.id}
               data={card}
             />)
+          })
         }
-
       </div>
-      <p id="sentinela"></p>
-     
     </>
   )
 }
